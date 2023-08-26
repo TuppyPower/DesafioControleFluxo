4c1963524bc42e1c284a4b57881d44c545762d51
Paulo Tuppy <130563312+TuppyPower@users.noreply.github.com>
2023-08-25 21:26:11 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import tech.cbm.dio.fluxo.exceptions.ParametrosInvalidosException;
+import tech.cbm.dio.fluxo.model.ContadorModel;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Digite o primeiro parâmetro: ");
+            int parametroUm = scanner.nextInt();
+
+            System.out.print("Digite o segundo parâmetro: ");
+            int parametroDois = scanner.nextInt();
+
+            ContadorModel model = new ContadorModel();
+            model.setParametros(parametroUm, parametroDois);
+            model.contar();
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
